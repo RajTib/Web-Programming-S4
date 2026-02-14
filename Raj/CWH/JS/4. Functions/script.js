2385af427bcc1da3f5a15bf9f076a8f94754627b
Raj <raj.tibarewala@gmail.com>
2026-02-14 22:45:00 +0530

    Updates

---
+++ Raj/CWH/JS/4. Functions/script.js
@@ -0,0 +1,17 @@
+function nice(name) {
+  console.log("Hey " + name + "! Good Morning");
+}
+
+nice("Raj");
+nice("Rohan");
+nice("Rehan");
+nice("Rahul");
+nice("Raman");
+
+const func1 = (x) => {
+  console.log("I am a arrow function", x);
+};
+
+func1(34);
+func1(66);
+func1(86);
---
+++ Raj/CWH/JS/6. Strings/index.js
@@ -0,0 +1,15 @@
+console.log("This is strings tutorial");
+
+console.log();
+
+let a = "Raj";
+console.log(a);
+
+console.log();
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+
+console.log();
