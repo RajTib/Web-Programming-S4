7a1d6b892f88ef4d8935607fb0e91d3cce94cca2
Astryx <raj.tibarewala@gmail.com>
2026-02-20 13:39:56 +0530

    Updates

--- Raj/CWH/JS/6. Strings/index.js
+++ Raj/CWH/JS/6. Strings/index.js
@@ -2,14 +2,41 @@ console.log("This is strings tutorial");
 
 console.log();
 
-let a = "Raj";
-console.log(a);
 
-console.log();
-
+// Accessing parts of strings
+a = "Yash"
 console.log(a[0]);
 console.log(a[1]);
 console.log(a[2]);
 console.log(a[3]);
 
 console.log();
+
+
+// Template Literals
+real_name = "Raj"
+age = 20
+console.log(`His name is ${real_name} and he is ${age} years old`)
+
+
+// Some Functions with Strings
+let b = "Shivam"
+console.log(b.toLowerCase());
+console.log(b.toUpperCase());
+console.log(b.length);
+
+
+// String Slicing
+console.log(b.slice(1, 4));
+console.log(b.slice(1, 5));
+console.log(b.slice(1));
+
+
+// Replacing parts of string
+console.log(b.replace("Sh", "77"));
+
+
+// Concatenating Strings
+let name1 = "Raj"
+let name2 = " Tibarewala"
+console.log(name1.concat(name2))
